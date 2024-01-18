7c6eafdab9b9ab080f770734cf65fd02d058df4e
shreenehaa <shreenehaa2003@gmail.com>
2024-01-18 17:55:51 +0530

    java script rest spread destructuring

---
+++ demo.js
@@ -0,0 +1,34 @@
+const movieURL = (domain, genre, year) => {
+  return "http://" + domain + "?genere=" + genre + "&year=" + year;
+};
+console.log(movieURL("imdb.com", "thiller", 2020));
+
+const movieURL1 = (domain, genre, year) => {
+  return `http://${domain}?genere=${genre}&year=${year}`;
+};
+console.log(movieURL1("imdb.com", "thiller", 2020));
+
+const movieURL2 = (domain, genre, year) =>
+  `http://${domain}?genere=${genre}&year=${year}`;
+console.log(movieURL2("imdb.com", "thiller", 2020));
+// spread operator
+let t1 = [100, 200];
+let t2 = [50, ...t1, 60];
+let t3 = [...t1];
+// it copies only the values
+console.log(t1 == t3);
+console.log(t2);
+
+// rest operator
+nums = [100, 200, 300];
+const [z1, ...z2] = nums;
+console.log(z1, z2);
+
+// object destructuring
+
+// const
+const { name, power } = {
+  name: "tony",
+  power: 100,
+};
+console.log(power);
